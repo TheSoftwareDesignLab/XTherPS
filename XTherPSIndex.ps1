c1c3a04b751a6a3386fee9218003cf3343502971
Dilan Steven Mejia Buitrago <dmejia@greensqa.com>
2019-02-13 10:11:12 -0500

    Creación del archivo XTherIndex

---
+++ XTherPSIndex.ps1
@@ -0,0 +1,48 @@
+#
+# MIT License
+#
+# Copyright 2019 GreenSQA S.A. & The software design LAB (Uniandes)
+#
+# Permission is  hereby granted,  free of charge,  to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software  without restriction, including without limitation 
+# the rights to use, copy, modify,  merge, publish,  distribute, sublicense, 
+# and/or sell copies  of the Software, and  to permit persons  to whom  the 
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright  notice and this permission notice  shall be included 
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT  LIMITED TO THE WARRANTIES  OF  MERCHANTABILITY, 
+# FITNESS FOR  A  PARTICULAR  PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL 
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR  ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH  THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN 
+# THE SOFTWARE.
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Script purpose:
+#
+#  Index of XTher Logic 
+#
+# @EiderMauricioAristizábalErazo
+# @DilanStevenMejiaBuitrago
+#
+
+
+$idxFileName = 0
+$idxFolder   = 1
+$idxFileUri   = 2
+
+
+$drvrDB = @"
+DALChrome.ps1;DAL;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/DAL/DALChrome.ps1?token=AHBiNw6nYI598XzmeSb3ugN6QSw535BSks5cbWqNwA%3D%3D
+DALEdge.ps1;DAL;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/DAL/DALEdge.ps1?token=AHBiN6-uAM-mQt5hsIE3ygMTX8rJY6kmks5cbWx-wA%3D%3D
+DALFireFox.ps1;DAL;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/DAL/DALFireFox.ps1?token=AHBiN6sq80o7LM4Sb4verzpjSbcZBiq6ks5cbWydwA%3D%3D
+DALStandAlone.ps1;DAL;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/DAL/DALStandAlone.ps1?token=AHBiNzYYtQHiQA1WCRMOr2uYJfU8L9qmks5cbWy2wA%3D%3D
+DALUriDB.ps1;DAL;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/DAL/DALUriDB.ps1?token=AHBiN13k0ci91NRBflQwjXYPyAt_Kg3kks5cbWzPwA%3D%3D
+SEResourcesDB.ps1;.;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/SEResourcesDB.ps1?token=AHBiN74oE2OFe1VYz5iXXkvFWETMhhmNks5cbW0iwA%3D%3D
+SEResourcesInstaller.ps1;.;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/SEResourcesInstaller.ps1?token=AHBiN4IO_4Ev0R8uL4XY5sxaRN_q5Ceqks5cbW0ywA%3D%3D
+
+"@
