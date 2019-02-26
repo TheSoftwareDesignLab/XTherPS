1ad464d6f0b1fde9fe1f05d07f7194e5af80bb4b
Eider Mauricio Aristizábal Erazo <earisti@greensqa.com>
2019-02-26 13:30:43 -0500

    disabled jacoco report, hosted windows lacks recent powershell

--- build.ps1
+++ build.ps1
@@ -9,7 +9,8 @@ function RunXTherPSTests()
     #Run tests with code coverage
     $CodeFiles = (Get-ChildItem $ProjectRoot/src -Recurse -Include "*.psm1","*.ps1").FullName
 
-    $Script:TestResults = Invoke-Pester -Path $ProjectRoot/tests -CodeCoverage $CodeFiles -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\testResults.xml" -ExcludeTag 'e2e' -CodeCoverageOutputFile "$ProjectRoot\codeCoverage.xml"
+    #$Script:TestResults = Invoke-Pester -Path $ProjectRoot/tests -CodeCoverage $CodeFiles -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\testResults.xml" -ExcludeTag 'e2e' -CodeCoverageOutputFile "$ProjectRoot\codeCoverage.xml"
+    $Script:TestResults = Invoke-Pester -Path $ProjectRoot/tests -CodeCoverage $CodeFiles -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\testResults.xml" -ExcludeTag 'e2e'
 
     #change location again
     Set-Location $ProjectRoot
