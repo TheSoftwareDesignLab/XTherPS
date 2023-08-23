75b732a02bcbcdcba32f233f2b63a3490254bd66
CarolinaDoncel <cdoncel@greensqa.com>
2023-08-23 10:00:22 -0500

    Se modifica el archivo XTherPSIndex

--- src/XTherPSIndex.ps1
+++ src/XTherPSIndex.ps1
@@ -41,7 +41,8 @@ DALChrome.ps1;/dal/;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTher
 DALEdge.ps1;/dal/;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/src/dal/DALEdge.ps1
 DALFireFox.ps1;/dal/;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/src/dal/DALFireFox.ps1
 DALStandAlone.ps1;/dal/;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/src/dal/DALStandAlone.ps1
-DALUriDB.ps1;/dal/;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/src/dal/DALUriDB.ps1
+DALUriDB.ps1;/dal/;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/f-10811/src/dal/DALUriDB.ps1
 SEResourcesDB.ps1;/;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/src/SEResourcesDB.ps1
-SEResourcesInstaller.ps1;/;https://raw.githubusercontent.com/TheSoftwareDesignLab/XTherPS/master/src/SEResourcesInstaller.ps1
+SEResourcesInstaller.ps1;/;https://github.com/TheSoftwareDesignLab/XTherPS/blob/f-10811/src/SEResourcesInstaller.ps1
 "@
+
